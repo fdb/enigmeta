7e33de3f76469673fc88f4765356393ee159806e
Frederik De Bleser <frederik@debleser.be>
2016-06-14 22:08:51 +0200

    Add thesis project.

---
+++ thesis/thesis.js
@@ -0,0 +1,16 @@
+var URL = 'http://gopub.enigmeta.com:3000/render';
+
+function onChangeStyle(e) {
+  loader.style.opacity = 1;
+  iframe.src = URL + '?style=' + e.target.value;
+}
+
+var loader = document.getElementById('loader');
+var styleSlider = document.getElementById('styleSlider');
+styleSlider.addEventListener('change', onChangeStyle);
+
+var iframe = document.querySelector('iframe');
+
+iframe.addEventListener('load', function() {
+  loader.style.opacity = 0;
+});
